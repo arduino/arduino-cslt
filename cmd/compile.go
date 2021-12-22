fd3cd7319d6156e4ca0e3c3bc2302a957ecd1365
umbynos <umberto.baldi@edu.unito.it>
2021-12-22 13:31:43 +0100

    make fqbn flag required and cleanup

--- cmd/compile.go
+++ cmd/compile.go
@@ -27,17 +27,8 @@ var compileCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(compileCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// compileCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// compileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	compileCmd.Flags().StringVarP(&fqbn, "fqbn", "b", "", "Fully Qualified Board Name, e.g.: arduino:avr:uno")
+	compileCmd.MarkFlagRequired("fqbn")
 }
 
 func compileSketch(cmd *cobra.Command, args []string) {
