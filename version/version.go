e22f7f3bb3535f4474e829b4e0d85aec8d7ed8d7
umbynos <umberto.baldi@edu.unito.it>
2022-01-24 18:36:50 +0100

    Add CI workflow to publish releases
    On every push of a tag named with a version format:
    
    - Build the project for all supported platforms.
    - Sign and notarize the macOS build.
    - Create a GitHub release.
      - Builds and checksums are attached as release assets
      - A changelog generated from the commit history is added to the release description
      - If the tag has a pre-release version suffix, the GitHub release will be marked as a pre-release.

---
+++ version/version.go
@@ -0,0 +1,10 @@
+package version
+
+// Version is the current version
+var Version = "unknown"
+
+// Commit is the current git tag
+var Commit = "unknown"
+
+// Timestamp is the current timestamp
+var Timestamp = "unknown"
